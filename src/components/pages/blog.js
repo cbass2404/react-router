8eb28a64b75a5956df5ea034c021cd2059955f8e
Cory Bass <cory.bass.07@gmail.com>
2020-10-20 23:22:05 -0500

    initial commit

---
+++ src/components/pages/blog.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Blog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Blog</div>;
+  }
+}
+
+export default Blog;
---
+++ src/components/pages/noMatch.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NoMatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>No Match</div>;
+  }
+}
+
+export default NoMatch;
